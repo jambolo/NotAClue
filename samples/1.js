dc34969b6e1aa41a8f8260c89d7e903b37dd31ac
jambolo <jambolo@users.noreply.github.com>
2018-12-08 10:34:35 -0800

    Updated sample logs

--- samples/1.js
+++ samples/1.js
@@ -1,27 +1,29 @@
-["joe","chris","dave","liz"]
-{"hand":{"player":"joe","cards":["plum","gray","brunette","pipe","dining","kitchen","studio"]}}
-{"suggest":{"player":"joe","cards":["mustard","knife","billiard"],"showed":["chris","liz"]}}
-{"show":{"player":"chris","card":"billiard"}}
-{"show":{"player":"liz","card":"mustard"}}
-{"suggest":{"player":"chris","cards":["peacock","poison","courtyard"],"showed":["dave","liz"]}}
-{"suggest":{"player":"dave","cards":["brunette","revolver","billiard"],"showed":["joe","chris"]}}
-{"suggest":{"player":"liz","cards":["brunette","knife","billiard"],"showed":["joe","chris"]}}
-{"suggest":{"player":"joe","cards":["white","rope","studio"],"showed":["chris","liz"]}}
-{"show":{"player":"chris","card":"rope"}}
-{"show":{"player":"liz","card":"white"}}
-{"suggest":{"player":"liz","cards":["plum","revolver","courtyard"],"showed":["joe"]}}
-{"suggest":{"player":"joe","cards":["scarlet","poison","billiard"],"showed":["chris","dave","liz"]}}
-{"show":{"player":"chris","card":"billiard"}}
-{"show":{"player":"dave","card":"scarlet"}}
-{"show":{"player":"liz","card":"poison"}}
-{"suggest":{"player":"chris","cards":["rose","wrench","courtyard"],"showed":["liz"]}}
-{"suggest":{"player":"dave","cards":["peach","horseshoe","courtyard"],"showed":["chris"]}}
-{"suggest":{"player":"liz","cards":["green","revolver","dining"],"showed":["joe","dave"]}}
-{"suggest":{"player":"joe","cards":["plum","pipe","courtyard"],"showed":[]}}
-{"suggest":{"player":"dave","cards":["white","rope","courtyard"],"showed":["chris","liz"]}}
-{"suggest":{"player":"liz","cards":["peach","revolver","courtyard"],"showed":["chris"]}}
-{"suggest":{"player":"joe","cards":["rose","pipe","courtyard"],"showed":[]}}
-{"suggest":{"player":"chris","cards":["rose","poison","courtyard"],"showed":["liz"]}}
-{"suggest":{"player":"dave","cards":["mustard","pipe","courtyard"],"showed":["joe","liz"]}}
-{"suggest":{"player":"liz","cards":["gray","candlestick","courtyard"],"showed":["joe","chris"]}}
-{"suggest":{"player":"joe","cards":["rose","revolver","courtyard"],"showed":[]}}
+[
+  { "setup" :   { "variation" : "master_detective", "players" : [ "joe", "chris", "dave", "liz" ]}},
+  { "hand" :    { "player" :    "joe",   "cards" : [ "plum", "gray", "brunette", "pipe", "dining", "kitchen", "studio" ]}},
+  { "suggest" : { "suggester" : "joe",   "cards" : [ "mustard", "knife", "billiard" ], "showed" : [ "chris", "liz" ]}},
+  { "show" :    { "player" :    "chris", "card" :  "billiard" }},
+  { "show" :    { "player" :    "liz",   "card" :  "mustard" }},
+  { "suggest" : { "suggester" : "chris", "cards" : [ "peacock", "poison", "courtyard" ], "showed" : [ "dave", "liz" ]}},
+  { "suggest" : { "suggester" : "dave",  "cards" : [ "brunette", "revolver", "billiard" ], "showed" : [ "joe", "chris" ]}},
+  { "suggest" : { "suggester" : "liz",   "cards" : [ "brunette", "knife", "billiard" ], "showed" : [ "joe", "chris" ]}},
+  { "suggest" : { "suggester" : "joe",   "cards" : [ "white", "rope", "studio" ], "showed" : [ "chris", "liz" ]}},
+  { "show" :    { "player" :    "chris", "card" :  "rope" }},
+  { "show" :    { "player" :    "liz",   "card" :  "white" }},
+  { "suggest" : { "suggester" : "liz",   "cards" : [ "plum", "revolver", "courtyard" ], "showed" : [ "joe" ]}},
+  { "suggest" : { "suggester" : "joe",   "cards" : [ "scarlet", "poison", "billiard" ], "showed" : [ "chris", "dave", "liz" ]}},
+  { "show" :    { "player" :    "chris", "card" :  "billiard" }},
+  { "show" :    { "player" :    "dave",  "card" :  "scarlet" }},
+  { "show" :    { "player" :    "liz",   "card" :  "poison" }},
+  { "suggest" : { "suggester" : "chris", "cards" : [ "rose", "wrench", "courtyard" ], "showed" : [ "liz" ]}},
+  { "suggest" : { "suggester" : "dave",  "cards" : [ "peach", "horseshoe", "courtyard" ], "showed" : [ "chris" ]}},
+  { "suggest" : { "suggester" : "liz",   "cards" : [ "green", "revolver", "dining" ], "showed" : [ "joe", "dave" ]}},
+  { "suggest" : { "suggester" : "joe",   "cards" : [ "plum", "pipe", "courtyard" ], "showed" : []}},
+  { "suggest" : { "suggester" : "dave",  "cards" : [ "white", "rope", "courtyard" ], "showed" : [ "chris", "liz" ]}},
+  { "suggest" : { "suggester" : "liz",   "cards" : [ "peach", "revolver", "courtyard" ], "showed" : [ "chris" ]}},
+  { "suggest" : { "suggester" : "joe",   "cards" : [ "rose", "pipe", "courtyard" ], "showed" : []}},
+  { "suggest" : { "suggester" : "chris", "cards" : [ "rose", "poison", "courtyard" ], "showed" : [ "liz" ]}},
+  { "suggest" : { "suggester" : "dave",  "cards" : [ "mustard", "pipe", "courtyard" ], "showed" : [ "joe", "liz" ]}},
+  { "suggest" : { "suggester" : "liz",   "cards" : [ "gray", "candlestick", "courtyard" ], "showed" : [ "joe", "chris" ]}},
+  { "suggest" : { "suggester" : "joe",   "cards" : [ "rose", "revolver", "courtyard" ], "showed" : []}}
+]
--- samples/2.js
+++ samples/2.js
@@ -1,31 +1,33 @@
-["Steejo","Rhapsody","Orbital","Shenryyr", "Aavak"]
-{"hand": {"player": "Steejo", "cards":["candlestick", "kitchen", "mustard"]}}
-{"suggest": {"player":"Steejo", "cards": ["plum", "revolver", "ballroom"], "showed" :["Orbital"]}} 
-{"show": {"player":"Orbital", "card":"revolver"}}
-{"suggest": {"player":"Rhapsody", "cards": ["scarlet", "revolver", "lounge"], "showed" :["Aavak", "Shenryyr"]}} 
-{"suggest": {"player":"Shenryyr", "cards": ["mustard", "revolver", "conservatory"], "showed" :["Steejo"]}} 
-{"suggest": {"player":"Orbital", "cards": ["mustard", "pipe", "dining"], "showed" :["Rhapsody"]}} 
-{"suggest": {"player":"Rhapsody", "cards": ["white", "wrench", "hall"], "showed" :["Aavak"]}} 
-{"suggest": {"player":"Aavak", "cards": ["white", "rope", "conservatory"], "showed" :["Shenryyr"]}} 
-{"suggest": {"player":"Shenryyr", "cards": ["scarlet", "rope", "billiard"], "showed" :["Steejo","Orbital"]}} 
-{"suggest": {"player":"Steejo", "cards": ["mustard", "candlestick", "ballroom"], "showed" :["Orbital","Rhapsody","Aavak"]}} 
-{"show": {"player":"Aavak", "card":"ballroom"}}
-{"suggest": {"player":"Orbital", "cards": ["plum", "knife", "library"], "showed" :["Rhapsody"]}} 
-{"suggest": {"player":"Rhapsody", "cards": ["scarlet", "candlestick", "conservatory"], "showed" :["Aavak", "Shenryyr","Steejo"]}} 
-{"suggest": {"player":"Aavak", "cards": ["scarlet", "candlestick", "ballroom"], "showed" :["Shenryyr","Steejo"]}} 
-{"suggest": {"player":"Shenryyr", "cards": ["peacock", "wrench", "library"], "showed" :["Steejo","Orbital","Rhapsody"]}} 
-{"suggest": {"player":"Steejo", "cards": ["mustard", "pipe", "kitchen"], "showed" :["Orbital","Rhapsody"]}} 
-{"show": {"player":"Rhapsody", "card":"pipe"}}
-{"suggest": {"player":"Orbital", "cards": ["mustard", "wrench", "dining"], "showed" :["Rhapsody","Aavak"]}} 
-{"suggest": {"player":"Rhapsody", "cards": ["mustard", "revolver", "ballroom"], "showed" :["Aavak"]}} 
-{"suggest": {"player":"Aavak", "cards": ["scarlet", "knife", "billiard"], "showed" :["Shenryyr","Steejo","Orbital"]}} 
-{"suggest": {"player":"Shenryyr", "cards": ["scarlet", "candlestick", "study"], "showed" :["Steejo"]}} 
-{"suggest": {"player":"Steejo", "cards": ["plum", "candlestick", "study"], "showed" :["Orbital"]}} 
-{"show": {"player":"Orbital", "card":"study"}}
-{"suggest": {"player":"Orbital", "cards": ["scarlet", "wrench", "kitchen"], "showed" :["Rhapsody", "Aavak", "Shenryyr","Steejo"]}} 
-{"suggest": {"player":"Rhapsody", "cards": ["scarlet", "wrench", "conservatory"], "showed" :[]}} 
-{"suggest": {"player":"Aavak", "cards": ["scarlet", "wrench", "ballroom"], "showed" :[]}} 
-{"suggest": {"player":"Shenryyr", "cards": ["scarlet", "wrench", "hall"], "showed" :["Steejo","Orbital","Rhapsody", "Aavak"]}} 
-{"suggest": {"player":"Steejo", "cards": ["mustard", "knife", "hall"], "showed" :["Orbital","Rhapsody"]}} 
-{"show": {"player":"Rhapsody", "card":"knife"}}
-{"suggest": {"player":"Orbital", "cards": ["scarlet", "revolver", "library"], "showed" :[]}} 
+[
+  { "setup" :   { "variation" : "classic", "players" : [ "Steejo", "Rhapsody", "Orbital", "Shenryyr", "Aavak" ]}},
+  { "hand" :    { "player" :    "Steejo",   "cards" : [ "candlestick", "kitchen", "mustard" ]}},
+  { "suggest" : { "suggester" : "Steejo",   "cards" : [ "plum", "revolver", "ballroom" ], "showed" : [ "Orbital" ]}},
+  { "show" :    { "player" :    "Orbital",  "card" :  "revolver" }},
+  { "suggest" : { "suggester" : "Rhapsody", "cards" : [ "scarlet", "revolver", "lounge" ], "showed" : [ "Aavak", "Shenryyr" ]}},
+  { "suggest" : { "suggester" : "Shenryyr", "cards" : [ "mustard", "revolver", "conservatory" ], "showed" : [ "Steejo" ]}},
+  { "suggest" : { "suggester" : "Orbital",  "cards" : [ "mustard", "pipe", "dining" ], "showed" : [ "Rhapsody" ]}},
+  { "suggest" : { "suggester" : "Rhapsody", "cards" : [ "white", "wrench", "hall" ], "showed" : [ "Aavak" ]}},
+  { "suggest" : { "suggester" : "Aavak",    "cards" : [ "white", "rope", "conservatory" ], "showed" : [ "Shenryyr" ]}},
+  { "suggest" : { "suggester" : "Shenryyr", "cards" : [ "scarlet", "rope", "billiard" ], "showed" : [ "Steejo", "Orbital" ]}},
+  { "suggest" : { "suggester" : "Steejo",   "cards" : [ "mustard", "candlestick", "ballroom" ], "showed" : [ "Orbital", "Rhapsody", "Aavak" ]}},
+  { "show" :    { "player" :    "Aavak",    "card" :  "ballroom" }},
+  { "suggest" : { "suggester" : "Orbital",  "cards" : [ "plum", "knife", "library" ], "showed" : [ "Rhapsody" ]}},
+  { "suggest" : { "suggester" : "Rhapsody", "cards" : [ "scarlet", "candlestick", "conservatory" ], "showed" : [ "Aavak", "Shenryyr", "Steejo" ]}},
+  { "suggest" : { "suggester" : "Aavak",    "cards" : [ "scarlet", "candlestick", "ballroom" ], "showed" : [ "Shenryyr", "Steejo" ]}},
+  { "suggest" : { "suggester" : "Shenryyr", "cards" : [ "peacock", "wrench", "library" ], "showed" : [ "Steejo", "Orbital", "Rhapsody" ]}},
+  { "suggest" : { "suggester" : "Steejo",   "cards" : [ "mustard", "pipe", "kitchen" ], "showed" : [ "Orbital", "Rhapsody" ]}},
+  { "show" :    { "player" :    "Rhapsody", "card" :  "pipe" }},
+  { "suggest" : { "suggester" : "Orbital",  "cards" : [ "mustard", "wrench", "dining" ], "showed" : [ "Rhapsody", "Aavak" ]}},
+  { "suggest" : { "suggester" : "Rhapsody", "cards" : [ "mustard", "revolver", "ballroom" ], "showed" : [ "Aavak" ]}},
+  { "suggest" : { "suggester" : "Aavak",    "cards" : [ "scarlet", "knife", "billiard" ], "showed" : [ "Shenryyr", "Steejo", "Orbital" ]}},
+  { "suggest" : { "suggester" : "Shenryyr", "cards" : [ "scarlet", "candlestick", "study" ], "showed" : [ "Steejo" ]}},
+  { "suggest" : { "suggester" : "Steejo",   "cards" : [ "plum", "candlestick", "study" ], "showed" : [ "Orbital" ]}},
+  { "show" :    { "player" :    "Orbital",  "card" :  "study" }},
+  { "suggest" : { "suggester" : "Orbital",  "cards" : [ "scarlet", "wrench", "kitchen" ], "showed" : [ "Rhapsody", "Aavak", "Shenryyr", "Steejo" ]}},
+  { "suggest" : { "suggester" : "Rhapsody", "cards" : [ "scarlet", "wrench", "conservatory" ], "showed" : []}},
+  { "suggest" : { "suggester" : "Aavak",    "cards" : [ "scarlet", "wrench", "ballroom" ], "showed" : []}},
+  { "suggest" : { "suggester" : "Shenryyr", "cards" : [ "scarlet", "wrench", "hall" ], "showed" : [ "Steejo", "Orbital", "Rhapsody", "Aavak" ]}},
+  { "suggest" : { "suggester" : "Steejo",   "cards" : [ "mustard", "knife", "hall" ], "showed" : [ "Orbital", "Rhapsody" ]}},
+  { "show" :    { "player" :    "Rhapsody", "card" : "knife" }},
+  { "suggest" : { "suggester" : "Orbital",  "cards" : [ "scarlet", "revolver", "library" ], "showed" : []}}
+]
